8e4f0b84821c527d20be1f160b17109d70fbe5dc
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-12-22 16:36:52 +0100

    feat: add project constants

---
+++ selenium/utils/constants.py
@@ -0,0 +1,29 @@
+"""
+This file contains constants used in the project.
+"""
+
+QUERY_KEYWORDS = {
+    "serviette femme",
+    "tampon femme",
+    "cup menstruelle",
+    "protege slip",
+    # "culotte menstruelle",
+}
+
+EXCLUDE_KEYWORDS = {
+    "cheveux",
+    "sport",
+    "rangement",
+    "bain",
+    "microfibre",
+    "douche",
+    "maquillage",
+    "plage",
+    "décoration",
+    "percer",
+    "manucure",
+    "trouse",
+    "stérilisateur",
+}
+
+TEST_SCRAPPING_ITEM_LIMIT = 50
