ab060d0538bc01eda48e968a8178d46cb0234dae
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-12-30 10:36:48 +0100

    feat(scraping): add ProductSpider class

--- scraping/spiders/product_page.py
+++ scraping/spiders/product_page.py
@@ -8,9 +8,12 @@ from urllib.parse import urljoin
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from scraping.utils.spiders import BaseSpider
+
 PATTERNS = SimpleNamespace(
     price_1="//span[contains(@class, 'apexPriceToPay')]//span[@class='a-offscreen']",
     price_2="//div[@data-feature-name='corePriceDisplay_desktop']//span[contains(@class, 'aok-offscreen')]",
+    price_3="//div[@data-feature-name='corePriceDisplay_desktop']//span[contains(@class, 'a-offscreen')]",
     unities="//table[@id='productDetails_techSpec_section_1']//th[text()=' Unités ']/following-sibling::td",
     feature_bullets="//div[@id='feature-bullets']//span[@class='a-list-item']",
     review_url="//a[@data-hook='see-all-reviews-link-foot']",
@@ -46,6 +49,18 @@ def get_price(driver: webdriver.Chrome) -> float | None:
         )
         return price
 
+    price = driver.find_elements(By.XPATH, PATTERNS.price_3)
+    if price:
+        price = price[0].get_attribute("textContent")
+        price = float(
+            str(price)
+            .strip()
+            .replace("\xa0", " ")
+            .split(" ", maxsplit=1)[0]
+            .replace(",", ".")
+        )
+        return price
+
     return None
 
 
@@ -56,7 +71,10 @@ def get_title(driver: webdriver.Chrome) -> str | None:
 
     title = driver.find_elements(By.ID, PATTERNS.title_id)
     if title:
-        return title[0].get_attribute("textContent")
+        title = title[0].get_attribute("textContent")
+        if title:
+            title = title.strip()
+            return title
 
     return None
 
@@ -97,6 +115,7 @@ def get_num_reviews(driver: webdriver.Chrome) -> int | None:
     """
 
     def split_num_reviews(num_reviews: str) -> int:
+        num_reviews = num_reviews.strip().replace("\xa0", " ")
         elems = num_reviews.split(" ")[:-1]
         if len(elems) == 1:
             return int(elems[0])
@@ -123,7 +142,7 @@ def get_feature_bullets(driver: webdriver.Chrome) -> list[str] | None:
         feature_bullets = [
             bullet.get_attribute("textContent") for bullet in feature_bullets
         ]
-        feature_bullets = [bullet for bullet in feature_bullets if bullet]
+        feature_bullets = [bullet.strip() for bullet in feature_bullets if bullet]
         return feature_bullets
 
     return None
@@ -156,3 +175,83 @@ def get_review_url(driver: webdriver.Chrome) -> str | None:
         return review_url
 
     return None
+
+
+class ProductPageSpider(BaseSpider):
+    """
+    A spider for scraping the product pages of the website.
+    """
+
+    def __init__(self, driver: webdriver.Chrome) -> None:
+        super().__init__(driver)
+        self.queue = []
+
+    def parse(self, url: str) -> dict:
+        """
+        Parse the product page.
+        """
+
+        self.driver.get(url)
+
+        product = {
+            "price": get_price(self.driver),
+            "title": get_title(self.driver),
+            "brand": get_brand(self.driver),
+            "avg_rating": get_avg_rating(self.driver),
+            "num_reviews": get_num_reviews(self.driver),
+            "feature_bullets": get_feature_bullets(self.driver),
+            "unities": get_unities(self.driver),
+            "review_url": get_review_url(self.driver),
+        }
+
+        return product
+
+    def query(self) -> list[str]:
+        """
+        Get the products to scrape.
+        """
+        pipeline = [
+            {"$match": {"_metadata.product_page_scraped": False}},
+            {"$project": {"asin": 1, "_id": 0}},
+        ]
+        items = list(self.mongodb.collection.aggregate(pipeline))
+        items = [doc["asin"] for doc in items]
+        self.queue = items
+        print(f"Found {len(items)} products to scrape.")
+        return items
+
+    def run(self) -> None:
+        """
+        Run the spider.
+        """
+
+        self.query()
+
+        def process_item(asin: str) -> int:
+            url = f"https://www.amazon.fr/dp/{asin}"
+            product = self.parse(url)
+            product["_metadata"] = {
+                "last_session_id": self.session_id,
+                "last_session_time": self.strtime,
+                "product_page_scraped": True,
+            }
+            self.mongodb.collection.update_one(
+                {"asin": asin},
+                {"$set": product},
+            )
+            return 1
+
+        counter = 0
+        for asin in self.queue:
+            counter += process_item(asin)
+            print(f"Updated {asin} --- Progress {counter}/{len(self.queue)}")
+
+        # Log the session.
+        ACTION_TYPE = "Product Page Scraping"
+
+        self.meta["action_type"] = ACTION_TYPE
+        self.meta["action_time"] = self.time
+        self.meta["update_count"] = counter
+
+        print(f"Scraped {counter} products.")
+        self.log()
