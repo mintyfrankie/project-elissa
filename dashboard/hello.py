a1c0325e65a25e6687886c100a7fed38bfb57ea2
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2023-12-31 21:29:26 +0100

    build(dashboard): add plotly

--- dashboard/hello.py
+++ dashboard/hello.py
@@ -2,6 +2,14 @@
 A demo dashboard for testing purposes, with Streamlit.
 """
 
+import pandas as pd
 import streamlit as st
 
-st.title("Hello, world!")
+st.title("Project Elissa - Dashboard")
+
+data_load_state = st.text("Loading data...")
+data = pd.read_csv("./data/products.csv")
+data_load_state.text("Loading data... done!")
+
+st.subheader("Raw data")
+st.write(data)
