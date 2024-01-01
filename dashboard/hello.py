9a79bc8e431a0ce08e9e8c67408c08dcf9195c3c
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2024-01-01 15:38:07 +0100

    feat(dashboard): add home page skeleton

--- dashboard/hello.py
+++ dashboard/hello.py
@@ -5,11 +5,43 @@ A demo dashboard for testing purposes, with Streamlit.
 import pandas as pd
 import streamlit as st
 
-st.title("Project Elissa - Dashboard")
 
-data_load_state = st.text("Loading data...")
-data = pd.read_csv("./data/products.csv")
-data_load_state.text("Loading data... done!")
+@st.cache_data
+def load_products():
+    return pd.read_csv("./data/products.csv")
 
-st.subheader("Raw data")
-st.write(data)
+
+@st.cache_data
+def load_reviews():
+    return pd.read_csv("./data/reviews.csv")
+
+
+with st.sidebar:
+    st.title("Project Elissa")
+    st.subheader("Navigation")
+    st.markdown(
+        """
+        - [Dashboard](/dashboard)
+        - [Recommender](/recommender)
+        - [About](/about)
+        """
+    )
+
+st.title("Dashboard")
+
+products = load_products()
+reviews = load_reviews()
+
+with st.container(border=True):
+    st.header("Metrics")
+    col1, col2 = st.columns(2)
+    col1.metric(label="Scraped Products", value=products.shape[0])
+    col2.metric(label="Scraped reviews", value=reviews.shape[0])
+
+with st.container(border=False):
+    st.header("Products")
+    st.dataframe(products)
+
+with st.container(border=False):
+    st.header("Reviews")
+    st.dataframe(reviews)
