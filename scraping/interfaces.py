0ba164c1ecfdcbeddca2075187dd36194ed54332
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2024-01-05 17:31:33 +0100

    feat(scraping): add BaseItem and ReviewItem classes to interfaces.py

--- scraping/interfaces.py
+++ scraping/interfaces.py
@@ -5,7 +5,7 @@ Interfaces for data validation in the pipeline.
 from datetime import datetime
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl, field_validator
 
 SCRAP_STATUS = Literal["SearchPage", "ProductPage", "ReviewPage"]
 
@@ -16,3 +16,43 @@ class ItemMetadata(BaseModel):
     last_session_id: int
     last_session_time: datetime
     scrap_status: SCRAP_STATUS
+
+
+class BaseItem(BaseModel):
+    """A base item added by SearchPageSpiderWorker"""
+
+    asin: str
+    title: str | None
+    thumbnail: HttpUrl | None
+    _metadata: ItemMetadata
+
+    @field_validator("asin")
+    @classmethod
+    def validate_asin(cls, v: str) -> str:
+        """Validates the ASIN"""
+        if not v.startswith("B0"):
+            raise ValueError("ASIN must start with 'B0'")
+        return v
+
+
+class ReviewItem(BaseModel):
+    """A review on Amazon."""
+
+    content: str | None
+    rating: int | None
+    title: str | None
+    location: str | None
+    date: str | None
+
+
+class ProductItem(BaseItem):
+    """A complete product document, extended by ProductPageSpiderWorker and ReviewPageSpiderWorker."""
+
+    brand: str
+    price: float | None
+    unities: float | None
+    avg_rating: float | None
+    num_reviews: int | None
+    features_bullets: list[str] | None
+    review_url: HttpUrl | None
+    reviews: list[ReviewItem] | None
