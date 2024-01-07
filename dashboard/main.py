23a5840b6d3f824ff10a6a8a370466ffaa8ce39d
yunan.wang <77310871+mintyfrankie@users.noreply.github.com>
2024-01-07 22:41:42 +0100

    feat: WIP

--- dashboard/main.py
+++ dashboard/main.py
@@ -7,41 +7,52 @@ TODO:
 
 import streamlit as st
 
-CATEGORIES = ("Tampon", "Serviette", "Cup", "Culotte", "Protège-slip", "Autre")
-FEATURES = ("Confort", "Écologie", "Absorption", "Prix", "Praticité", "Autre")
-
-with st.sidebar:
-    st.subheader("Category")
-    category_option = st.radio("Select a category", CATEGORIES, horizontal=True)
-
-    st.subheader("Features")
-    features_option = st.multiselect("Select features", FEATURES)
-
-    with st.container():
-        st.subheader("Price range")
-        price_range = st.slider("Select a price range", 0, 100, (25, 75))
-
 
 def product_card():
     with st.container(border=True) as card:
         st.image("https://m.media-amazon.com/images/I/81mAiEAY81L._AC_UL320_.jpg")
-        st.write(
-            "Nana Dailies Style Protège-Lingerie Multistyle - Protège-Slip Fin et Ultra-Absorbant pour Tous Types de Lingerie - 30 en Pochette Individuelle"
-        )
-        st.write("Évaluation : 4.4 / 5 -- 806 avis")
-        st.write("Prix : 1,73€")
-        st.write("Tags : Confort, Praticité, Prix")
+        st.subheader("Nana Dailies Style Protège-Lingerie Multistyle...")
+        st.write("*Évaluation : 4.4 / 5 -- 806 avis*")
+        st.write("*Prix : 1,73€*")
+        st.write("*Tags : Confort, Praticité, Prix*")
         return card
 
 
-st.subheader("Presentation Example")
+CATEGORIES = ("Tampon", "Serviette", "Cup", "Culotte", "Protège-slip", "Autre")
+FEATURES = ("Confort", "Écologie", "Absorption", "Prix", "Praticité", "Autre")
+
+st.header("Project Elissa")
+
+st.empty()
+
+
+st.subheader("Query", divider=True)
+
 with st.container():
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2 = st.columns(2)
+
     with col1:
-        product_card()
+        category_option = st.radio("Category", CATEGORIES, horizontal=True)
+
+        features_option = st.multiselect("Features", FEATURES)
+
     with col2:
-        product_card()
-    with col3:
-        product_card()
-    with col4:
-        product_card()
+        price_range = st.slider("Price range", 0, 100, (25, 75))
+
+
+st.subheader("Result", divider=False)
+
+tab1, tab2 = st.tabs(["Cards", "Table"])
+
+with tab1:
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            product_card()
+        with col2:
+            product_card()
+        with col3:
+            product_card()
+
+with tab2:
+    pass
